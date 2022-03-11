594d477cec807c8892d88a43580ac812f0d95cfb
Jack Gamboa <jgamboa929@gmail.com>
2022-03-11 11:33:57 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Simple Questions Project with React
+
+Simple Questions project with React. Display/hide info feature.
+
+![react-simple-accordion](https://user-images.githubusercontent.com/43181662/157918027-94c4e9d6-78d8-4782-81dd-707e9f9ba73c.png)
+
+## Technologies
+
+:coffee: **_React_**
---
+++ src/Question.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+// import { AiOutlineMinusCircle } from "react-icons/ai";
+import { FiMinus, FiPlus } from "react-icons/fi";
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <FiMinus /> : <FiPlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Question;
